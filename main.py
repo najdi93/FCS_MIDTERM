d96b2df71070f79bd5aa2990bb64c6d939c9800a
mohammad <mohammadnajdi.14@gmail.com>
2023-08-21 20:29:19 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print ( " hello mohohammad najdi ")
+# Ask the user for the filename
+filename = input("Please enter the name of the text file to import: ")
+
+# Open the text file in read mode
+with open(filename, 'r') as file:
+    content = file.read()
+
+
+from tkinter import *
+
+MAX_ATTEMPTS = 5
+
+attempts = 0
+
+def login():
+    global attempts
+    username = username_entry.get()
+    password = password_entry.get()
